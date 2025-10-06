3faa48e279a7933d5cd670e0934a414d64986aed
Moinuddin-dotcom <moinchy7@gmail.com>
2025-10-06 11:43:14 +0600

    padding remove

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function HomePage() {
   // };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 space-y-6">
+    <div className="max-w-4xl mx-auto px-6 pt-6 space-y-6">
       <h1 className="text-2xl font-bold">🔋 Site Passive Measurement</h1>
 
       {/* Form */}
